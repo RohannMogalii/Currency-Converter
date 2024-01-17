c642e166eb8b0d6c228d31a03aa5b27ab73fcaf3
Rohan <mogalirohan24@gmail.com>
2024-01-18 00:29:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+let msg = document.querySelector(".msg");
+
+for(let select of dropdowns){
+    for(currCode in countryList){
+        let newOption = document.createElement("option");
+        newOption.innerText=currCode;
+        newOption.value=currCode;
+        if(select.name === "from" && currCode === "USD" ){
+            newOption.selected="selected";
+        }else if(select.name === "to" && currCode === "INR"){
+            newOption.selected="selected";
+        }
+        select.append(newOption);
+    }
+    select.addEventListener("change",(evt)=>{
+        updateFlag(evt.target);
+    });
+}
+
+const updateFlag = (element)=>{
+    // console.log(element.value);
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    console.log(countryCode);
+
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img =  element.parentElement.querySelector("img");
+    img.src = newSrc;
+}
+
+btn.addEventListener("click",async (evt)=>{
+    evt.preventDefault();
+
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+    console.log(amtVal);
+    if(amtVal < 1 || amtVal ===""){
+        alert("Enter a valid amount!");
+        amtVal=1;
+        amount.value="1";
+    }
+    
+    // console.log(fromCurr.value.toLowerCase());
+    // console.log(toCurr.value.toLowerCase())
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+
+    let response = await fetch(URL);
+    // console.log(response);
+    let data = await response.json();
+    // console.log(data);
+    let rate = data[toCurr.value.toLowerCase()];
+    // console.log(rate);
+    let finalAmt = rate * amtVal;
+    // console.log(finalAmt);
+    msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmt} ${toCurr.value}`;
+})
